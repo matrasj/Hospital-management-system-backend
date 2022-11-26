640636dc83fe7b0fa02d31b949b9b0889f5904a8
pozerinhooo <jkob.matras@gmail.com>
2022-11-26 23:30:33 +0100

    Created entities responsible for authentication

---
+++ src/main/java/com/example/hospitalmanagementsystembackend/jwt/JwtTokenProvider.java
@@ -0,0 +1,4 @@
+package com.example.hospitalmanagementsystembackend.jwt;
+
+public class JwtUtils {
+}
---
+++ src/main/java/com/example/hospitalmanagementsystembackend/model/entity/Role.java
@@ -0,0 +1,2 @@
+package com.example.hospitalmanagementsystembackend.model.entity;public class Role {
+}
