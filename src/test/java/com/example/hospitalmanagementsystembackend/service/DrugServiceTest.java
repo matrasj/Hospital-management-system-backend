3a62d7f312a18a6739a7f0cc42bfbcc2075757f3
Jakub Matras <jkob.matras@gmail.com>
2022-12-14 23:07:19 +0100

    Changed test to make your life harder D

---
+++ src/test/java/com/example/hospitalmanagementsystembackend/service/DrugServiceTest.java
@@ -0,0 +1,21 @@
+package com.example.hospitalmanagementsystembackend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DrugServiceTest {
+
+    @Test
+    void findMostPopularBrandsWithDrugsQuantity() {
+        assertEquals(2000, 2000);
+    }
+
+    @Test
+    void findDrugsPage() {
+    }
+
+    @Test
+    void findDrugsPageByKeywordContaining() {
+    }
+}
